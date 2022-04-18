4e0d6bdaa85d1578544a68d8fbb3e960ca29114a
kmmontes <kmontes4989@gmail.com>
2022-04-18 14:52:16 -0700

    Initial commit

---
+++ text-based-game-ja-sk-kl.rb
@@ -0,0 +1,63 @@
+# #STORYBACKGROUND#
+# ************************************************************************************
+# #  p 'The young wizard has to kill the monster Chupacabra in order to save his town/village. Harry, The young wizard, is armed with his magical wand, library of kill spells and a desire for vengeance. The Chupacabra known for attacking its prey with its deadly fangs and sucking its preys blood to death, has entered Harry’s village after a spell was cast to destroy it by a rival evil wizard. The Chupacabra has terrorized  the townspeople and killed Harry’s parents in one its major recent attacks. Let’s help Harry find the Chupacabra in his hiding place - the towns abandoned mine.'
+
+# # puts "Harry enters the mine"
+# # harry=gets.chomp
+# ***********************************************************************************************
+
+def entrance
+
+puts 'Press [ENTER] to begin.'
+ready =gets.chomp
+puts
+puts
+
+puts 'Harry walks in to the pitch black mine and hears a sound'
+puts
+
+puts '1. ignore' 
+puts '2. investigate sound'
+
+
+sound = gets.chomp
+puts
+    if sound == '1'
+         puts "Harry enters the army of evil skeletons waiting to attack and is instantly killed"
+    elsif 
+        puts  "Harry notices and avoids a trap of army of evil skeletons awaiting him killing them all with  Harry's spell avid kedarva."
+end 
+end 
+
+puts 'Press [ENTER] to begin.'
+puts
+puts "Harry kills the evil army of skeletons and continues down the mine. He sees an amber colored faint light and hears screams and roars"
+puts
+puts
+puts '1. investigate roar and light' 
+puts '2. walk back in shame you coward'
+puts
+puts
+def mine = gets.chomp
+    if mine == '1'
+        puts "Harry continues down the mine and spots the chupcabra in a deep sleep."
+    elsif 
+        puts "Harry runs back out of the mine, steps on a mine explosion, and dies instantly"
+end
+
+
+puts
+puts 
+puts "Harry spots and approaches the chupacabra"
+puts
+puts '1. spots chupcabra'
+puts '2. get close to chupcabra'
+puts
+def spots = gets.chomp
+    if spots == '1'
+        puts "Harry spots the chupacabra in a deep sleep and approaches to begin attack"
+    elsif
+    puts "Harry gets close and falls before he can deploy the spell on the sound asleep chupcabra. Harry is instantly killed."
+end 
+
+
