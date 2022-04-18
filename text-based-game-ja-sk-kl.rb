3dc0d4f77a9dfecca6982009d2d8b1712d1115e5
joagui83 <jjaggie83@gmail.com>
2022-04-18 16:11:06 -0700

    Finished the story process. Working on setting up the methods.

--- text-based-game-ja-sk-kl.rb
+++ text-based-game-ja-sk-kl.rb
@@ -6,58 +6,88 @@
 # # harry=gets.chomp
 # ***********************************************************************************************
 
-def entrance
 
-puts 'Press [ENTER] to begin.'
-ready =gets.chomp
-puts
-puts
-
-puts 'Harry walks in to the pitch black mine and hears a sound'
-puts
-
-puts '1. ignore' 
-puts '2. investigate sound'
-
-
-sound = gets.chomp
-puts
-    if sound == '1'
-         puts "Harry enters the army of evil skeletons waiting to attack and is instantly killed"
-    elsif 
-        puts  "Harry notices and avoids a trap of army of evil skeletons awaiting him killing them all with  Harry's spell avid kedarva."
-end 
-end 
-
-puts 'Press [ENTER] to begin.'
-puts
-puts "Harry kills the evil army of skeletons and continues down the mine. He sees an amber colored faint light and hears screams and roars"
-puts
-puts
-puts '1. investigate roar and light' 
-puts '2. walk back in shame you coward'
-puts
-puts
-def mine = gets.chomp
-    if mine == '1'
-        puts "Harry continues down the mine and spots the chupcabra in a deep sleep."
-    elsif 
-        puts "Harry runs back out of the mine, steps on a mine explosion, and dies instantly"
+def test_method
+    puts 'Press [ENTER] to begin.'
+    ready =gets.chomp
+    puts 'Harry walks in to the pitch black mine and hears a sound'
+    puts
+    puts '1. ignore' 
+    puts '2. investigate sound'
+    sound = gets.chomp
+    puts
+        if sound == '1'
+            puts "Harry enters the army of evil skeletons waiting to attack and is instantly killed"
+            p test_method
+        elsif 
+            
+            p method_test2
+    end 
 end
 
 
-puts
-puts 
-puts "Harry spots and approaches the chupacabra"
-puts
-puts '1. spots chupcabra'
-puts '2. get close to chupcabra'
-puts
-def spots = gets.chomp
-    if spots == '1'
-        puts "Harry spots the chupacabra in a deep sleep and approaches to begin attack"
-    elsif
-    puts "Harry gets close and falls before he can deploy the spell on the sound asleep chupcabra. Harry is instantly killed."
-end 
+def method_test2
+    puts "Harry notices and avoids a trap of army of evil skeletons awaiting him killing them all with  Harry's spell avid kedarva.Harry kills the evil army of skeletons and continues down the mine. He sees an amber colored faint light and hears screams and roars"
+
+    puts '1. walk back in shame you coward' 
+    puts '2. investigate roar and light'
+        mine = gets.chomp
+        if mine == '1'
+            puts "Harry runs back out of the mine, steps on a mine explosion, and dies instantly."
+            p test_method
+        elsif 
+            p method_test3
+        end
+end
+
+
+def method_test3
+    puts "Harry spots and approaches the chupacabra"
+    puts '1. spots chupcabra'
+    puts '2. get close to chupcabra'
+    spots = gets.chomp
+        if spots == '1'
+            puts "Harry gets close and falls before he can deploy the spell on the sound asleep chupcabra. Harry is instantly killed."
+            test_method
+        elsif
+            puts "Harry spots the chupacabra in a deep sleep and approaches to begin attack"
+        end 
+end
+
+
+
+# puts
+# puts "if slowly approach chupacabra = Harry slowly approached deploys his spell avid kedarava and the chupacabra immediately wakes up furious looking for a hiding Harry. The Chupacabra finds Harry hiding behind a rock and the battle begins. "
+# puts
+# puts '1. Harry uses wand '
+# puts '2. Harry uses rock to defend'
+# puts
+# puts
+# battle = gets.chomp
+
+#     if battle = "1"
+#         puts "If Harry use wand =The chupacabra with his powerful leap, aims for Harrys neck using his poisonous fangs and begins to ferociously attack Harry. Harry drops his wand -his only weapon aside from vengeance. "
+#     elsif 
+#         puts "The chupacabra sucks Harrys blood killing him instantly."
+#     end
+# puts
+# puts
+# puts
+# puts '1 accetp defeat'
+# puts '2 reach for rock'
+# puts 
+# puts 
+# rock = gets.chomp
+#     if rock == '1'
+#         p "Chupcabra kills Harry instantly."
+#     elsif 
+#         puts"chupcabra is killed instantly with the spell of doom and destruction handed to him by Dumbledore- Movades grandtanium. Which consists of englufing the entire mine in flames." 
+#     end
+
+
+p test_method
+
+
+
 
 
